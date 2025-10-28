0ad9f4f8338e6f9618b8e379a327d95132e792b2
akashmngkmit <akashnandan@gkmit.co>
2025-10-28 16:06:54 +0530

    test(n8n): add console line

--- test/demo.js
+++ test/demo.js
@@ -1,3 +1,4 @@
 console.log("This is a demo test file for N8N.");
 console.log("This is a demo test file for listening at N8N.");
+console.log("This is a demo test file for testing at N8N.");
 // You can add your test cases here.
